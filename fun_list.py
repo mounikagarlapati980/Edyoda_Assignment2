24552bd870727f494a56f2c62fa5a6fe6fa433db
Mounika Garlapati <75879073+mounikagarlapati980@users.noreply.github.com>
2022-07-23 09:22:26 +0530

    New assignment2 question 1

--- fun_list.py
+++ fun_list.py
@@ -1,14 +1,12 @@
 size=int(input("Enter Size:"))
 lst=[]
 for i in range(size):
-    lst1=[]
-    for j in range(2):
-        value=int(input("Enter value"))
-        lst1.append(value)
-    t=tuple(lst1)
-    lst.append(t)
+    value1=int(input("Enter value1"))
+    value2=int(input("Enter value2"))
+    lst.append((value1,value2))
 for i in range(len(lst)):
     for j in range(len(lst)-i-1):
         if lst[j][1]>lst[j+1][1]:
             lst[j],lst[j+1]=lst[j+1],lst[j]
 print(lst)
+
